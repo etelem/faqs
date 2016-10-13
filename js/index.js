e61a4395c42a637077bc1328e076b15d600008bd
Elan Telem <e225mt@gmail.com>
2016-10-12 21:15:48 -0400

    FAQ's

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// Tip: You can either hide the answer on page load with js or use display:none in css
+
+
+
+$("#question1").on("click", function() {
+$("#answer1").toggle("#answer1");
+
+});
+
+$("#question2").on("click", function() {
+$("#answer2").toggle("#answer2");
+
+});
+
+$("#question3").on("click", function() {
+$("#answer3").toggle("#answer3");
+
+});
